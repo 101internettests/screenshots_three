481602bf9556c7359f8a0aac341ee068b656511d
EkaterinaMangileva <mangileva.ek@gmail.com>
2024-05-27 18:13:15 +0500

    добавила еще пару тестов

--- tests/101/test_main.py
+++ tests/101/test_main.py
@@ -126,3 +126,61 @@ class TestSomePage(ImageProcessor):
         image_101.show()
         image_pol.show()
         image_result.show()
+
+    @allure.title("Сравнение страницы 'тарифы'")
+    def test_rates(self, driver: webdriver.Chrome):
+        path = '/rates'
+        driver.get(host_101 + path)
+        driver.set_window_size(self.WINDOW_H, self.WINDOW_W)
+
+        second_driver = create_screenshot_driver()
+        second_driver.get(host_pol + path)
+        second_driver.set_window_size(self.WINDOW_H, self.WINDOW_W)
+
+        third_driver = create_screenshot_driver()
+        third_driver.get(host_mol + path)
+        third_driver.set_window_size(self.WINDOW_H, self.WINDOW_W)
+
+        screenshot_101_bytes = driver.get_screenshot_as_png()
+        screenshot_pol_bytes = second_driver.get_screenshot_as_png()
+        screenshot_mol_bytes = third_driver.get_screenshot_as_png()
+
+        image_101 = self.load_image_from_bytes(screenshot_101_bytes)
+        image_pol = self.load_image_from_bytes(screenshot_pol_bytes)
+        image_mol = self.load_image_from_bytes(screenshot_mol_bytes)
+
+        get_result = self.check_images_difference(image_101, image_mol, image_pol)
+        image_result = self.load_image_from_bytes(get_result[1])
+        image_mol.show()
+        image_101.show()
+        image_pol.show()
+        image_result.show()
+
+    @allure.title("Сравнение страницы 'интернет в офис'")
+    def test_orders_office(self, driver: webdriver.Chrome):
+        path = '/orders/office'
+        driver.get(host_101 + path)
+        driver.set_window_size(self.WINDOW_H, self.WINDOW_W)
+
+        second_driver = create_screenshot_driver()
+        second_driver.get(host_pol + path)
+        second_driver.set_window_size(self.WINDOW_H, self.WINDOW_W)
+
+        third_driver = create_screenshot_driver()
+        third_driver.get(host_mol + path)
+        third_driver.set_window_size(self.WINDOW_H, self.WINDOW_W)
+
+        screenshot_101_bytes = driver.get_screenshot_as_png()
+        screenshot_pol_bytes = second_driver.get_screenshot_as_png()
+        screenshot_mol_bytes = third_driver.get_screenshot_as_png()
+
+        image_101 = self.load_image_from_bytes(screenshot_101_bytes)
+        image_pol = self.load_image_from_bytes(screenshot_pol_bytes)
+        image_mol = self.load_image_from_bytes(screenshot_mol_bytes)
+
+        get_result = self.check_images_difference(image_101, image_mol, image_pol)
+        image_result = self.load_image_from_bytes(get_result[1])
+        image_mol.show()
+        image_101.show()
+        image_pol.show()
+        image_result.show()
